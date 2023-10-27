132011b8c8dd37185a4d5ae6f3a294d7327b03d4
Rostislav Podgorniy <podgorniy.inc@gmail.com>
2023-10-27 19:42:20 +0200

    formatting

--- lua/podgorniy/plugins/nvim-cmp.lua
+++ lua/podgorniy/plugins/nvim-cmp.lua
@@ -1,5 +1,5 @@
 -- +
-return {    -- :h completeopt
+return { -- :h completeopt
   "hrsh7th/nvim-cmp",
   event = "InsertEnter",
   dependencies = {
@@ -21,7 +21,7 @@ return {    -- :h completeopt
     require("luasnip.loaders.from_vscode").lazy_load()
 
     cmp.setup({
-      completion = {    -- configure how the completion menu works
+      completion = { -- configure how the completion menu works
         completeopt = "menu,menuone,preview,noselect",
       },
       snippet = { -- configure how nvim-cmp interacts with snippet engine
