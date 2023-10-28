b011d3cd4bac58430f8f36133e7ba927e469d628
Rostislav Podgorniy <podgorniy.inc@gmail.com>
2023-10-28 21:38:49 +0200

    markdown-preview fixed

--- lua/podgorniy/plugins/markdown-preview.lua
+++ lua/podgorniy/plugins/markdown-preview.lua
@@ -1,8 +1,11 @@
 return {
-  {
-    'iamcco/markdown-preview.nvim',
-    ft = 'markdown',
-    lazy = true,
-    keys = { { 'gm', '<cmd>MarkdownPreviewToggle<cr>', desc = 'Markdown Preview' } },
-  },
+  'iamcco/markdown-preview.nvim',
+  cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
+  lazy = true,
+  keys = { { 'gm', '<cmd>MarkdownPreviewToggle<cr>', desc = 'Markdown Preview' } },
+  build = 'cd app && yarn install',
+  init = function()
+    vim.g.mkdp_filetypes = { 'markdown' }
+  end,
+  ft = { 'markdown' },
 }
