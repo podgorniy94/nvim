9097fbd1e4730c7cc56580532c995a8b46936d7b
Rostislav Podgorniy <podgorniy.inc@gmail.com>
2023-10-28 22:38:56 +0200

    django linting/formatting + final upd

--- lua/podgorniy/plugins/formatting.lua
+++ lua/podgorniy/plugins/formatting.lua
@@ -7,19 +7,15 @@ return {
 
     conform.setup({
       formatters_by_ft = {
-        -- javascript = { "prettier" },
-        -- typescript = { "prettier" },
         -- javascriptreact = { "prettier" },
         -- typescriptreact = { "prettier" },
-        -- svelte = { "prettier" },
         css = { 'prettier' },
         html = { 'prettier' },
         json = { 'prettier' },
-        -- yaml = { "prettier" },
         markdown = { 'prettier' },
-        -- graphql = { "prettier" },
         lua = { 'stylua' },
         python = { 'isort', 'black' }, -- Executed in the order listed in the table
+        htmldjango = { 'djlint' },
       },
       format_on_save = {
         lsp_fallback = true, -- Try to format via LSP if the formatter isn't available
