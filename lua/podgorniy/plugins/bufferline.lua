ce45c01a2c479276f000ae8c5044ab6ab09f25e7
podgorniy94 <podgorniy.inc@gmail.com>
2024-02-15 17:10:19 +0100

    editing

--- lua/podgorniy/plugins/bufferline.lua
+++ lua/podgorniy/plugins/bufferline.lua
@@ -5,8 +5,8 @@ return {
   opts = {
     options = {
       mode = 'tabs',
-      --      separator_style = "slant",
-      show_buffer_close_icons = false,
+      -- separator_style = 'slant',
+      showmbuffer_close_icons = false,
       show_close_icon = false,
       color_icons = false,
     },
