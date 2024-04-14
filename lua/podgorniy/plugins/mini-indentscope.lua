d4f4213ecc73a77898e3d58222746205593bae1c
podgorniy94 <podgorniy.inc@gmail.com>
2024-04-14 23:50:08 +0200

    excluding python, lua indentation

--- lua/podgorniy/plugins/mini-indentscope.lua
+++ lua/podgorniy/plugins/mini-indentscope.lua
@@ -5,4 +5,34 @@ return {
     symbol = '│',
     options = { try_as_border = true },
   },
+  config = function(_, opts)
+    require('mini.indentscope').setup(opts)
+
+    -- Disable for certain filetypes
+    vim.api.nvim_create_autocmd({ 'FileType' }, {
+      desc = 'Disable indentscope for certain filetypes',
+      callback = function()
+        local ignore_filetypes = {
+          'aerial',
+          'dashboard',
+          'help',
+          'lazy',
+          'leetcode.nvim',
+          'mason',
+          'neo-tree',
+          'NvimTree',
+          'neogitstatus',
+          'notify',
+          'startify',
+          'toggleterm',
+          'python',
+          'lua',
+          'Trouble',
+        }
+        if vim.tbl_contains(ignore_filetypes, vim.bo.filetype) then
+          vim.b.miniindentscope_disable = true
+        end
+      end,
+    })
+  end,
 }
