906e4cb4ae2392b4f7d1f6a6136076b0428ae6da
podgorniy94 <podgorniy.inc@gmail.com>
2024-05-18 15:17:26 +0200

    errors solved with new neovim

--- lua/podgorniy/plugins/bufferline.lua
+++ lua/podgorniy/plugins/bufferline.lua
@@ -1,14 +1,12 @@
 return {
   'akinsho/bufferline.nvim',
   dependencies = { 'nvim-tree/nvim-web-devicons' },
-  version = '*',
+  -- tag = '*',
+  branch = 'main',
   opts = {
     options = {
       mode = 'tabs',
       separator_style = 'slant',
-      showmbuffer_close_icons = false,
-      show_close_icon = false,
-      color_icons = false,
     },
   },
 }
