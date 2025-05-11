bd4ba79a2d1ee6bb5cdda348a1124e8228894f6b
podgorniy94 <podgorniy.inc@gmail.com>
2025-05-11 13:36:56 +0300

    fix(lsp): replace deprecated sign_define with diagnostic.config

--- lua/podgorniy/plugins/lsp/lspconfig.lua
+++ lua/podgorniy/plugins/lsp/lspconfig.lua
@@ -71,13 +71,16 @@ return {
     -- used to enable autocompletion (assign to every lsp server config)
     local capabilities = cmp_nvim_lsp.default_capabilities()
 
-    -- Change the Diagnostic symbols in the sign column (gutter)
-    -- (not in youtube nvim video)
-    local signs = { Error = ' ', Warn = ' ', Hint = '󰠠 ', Info = ' ' }
-    for type, icon in pairs(signs) do
-      local hl = 'DiagnosticSign' .. type
-      vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = '' })
-    end
+    vim.diagnostic.config({
+      signs = {
+        text = {
+          [vim.diagnostic.severity.ERROR] = ' ',
+          [vim.diagnostic.severity.WARN]  = ' ',
+          [vim.diagnostic.severity.HINT]  = '󰠠 ',
+          [vim.diagnostic.severity.INFO]  = ' ',
+        },
+      },
+    })
 
     -- if you want to debug
     vim.lsp.set_log_level('debug')
