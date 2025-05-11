830cc88d70e0595128ee37732bdb2f579d130509
podgorniy94 <podgorniy.inc@gmail.com>
2025-05-11 15:45:01 +0300

    chore(lsp): update mason-lspconfig to use new setup_handlers method chore(telescope): update to latest version chore(ui): remove bufferline due to unstable behavior

--- lua/podgorniy/plugins/lsp/lspconfig.lua
+++ lua/podgorniy/plugins/lsp/lspconfig.lua
@@ -85,52 +85,55 @@ return {
     -- if you want to debug
     vim.lsp.set_log_level('debug')
 
-    mason_lspconfig.setup_handlers({
-      -- default handler for installed servers
-      function(server_name)
-        lspconfig[server_name].setup({
-          capabilities = capabilities,
-        })
-      end,
-      ['emmet_ls'] = function()
-        -- configure emmet language server
-        lspconfig['emmet_ls'].setup({
-          capabilities = capabilities,
-          filetypes = { 'html', 'typescriptreact', 'javascriptreact', 'css', 'sass', 'scss', 'less', 'svelte' },
-        })
-      end,
-      ['lua_ls'] = function()
-        -- configure lua server (with special settings)
-        lspconfig['lua_ls'].setup({
-          capabilities = capabilities,
-          settings = {
-            Lua = {
-              -- make the language server recognize "vim" global
-              diagnostics = {
-                globals = { 'vim' },
-              },
-              completion = {
-                callSnippet = 'Replace',
+    -- Mason-lspconfig ≥ 2.x: новый способ указать handlers
+    mason_lspconfig.setup({
+      handlers = {
+        -- default handler for installed servers
+        function(server_name)
+          lspconfig[server_name].setup({
+            capabilities = capabilities,
+          })
+        end,
+        ['emmet_ls'] = function()
+          -- configure emmet language server
+          lspconfig['emmet_ls'].setup({
+            capabilities = capabilities,
+            filetypes = { 'html', 'typescriptreact', 'javascriptreact', 'css', 'sass', 'scss', 'less', 'svelte' },
+          })
+        end,
+        ['lua_ls'] = function()
+          -- configure lua server (with special settings)
+          lspconfig['lua_ls'].setup({
+            capabilities = capabilities,
+            settings = {
+              Lua = {
+                -- make the language server recognize "vim" global
+                diagnostics = {
+                  globals = { 'vim' },
+                },
+                completion = {
+                  callSnippet = 'Replace',
+                },
               },
             },
-          },
-        })
-      end,
-      ['svelte'] = function()
-        -- configure svelte server
-        lspconfig['svelte'].setup({
-          capabilities = capabilities,
-          on_attach = function(client, bufnr)
-            vim.api.nvim_create_autocmd('BufWritePost', {
-              pattern = { '*.js', '*.ts' },
-              callback = function(ctx)
-                -- Here use ctx.match instead of ctx.file
-                client.notify('$/onDidChangeTsOrJsFile', { uri = ctx.match })
-              end,
-            })
-          end,
-        })
-      end,
+          })
+        end,
+        ['svelte'] = function()
+          -- configure svelte server
+          lspconfig['svelte'].setup({
+            capabilities = capabilities,
+            on_attach = function(client, bufnr)
+              vim.api.nvim_create_autocmd('BufWritePost', {
+                pattern = { '*.js', '*.ts' },
+                callback = function(ctx)
+                  -- Here use ctx.match instead of ctx.file
+                  client.notify('$/onDidChangeTsOrJsFile', { uri = ctx.match })
+                end,
+              })
+            end,
+          })
+        end,
+      },
     })
   end,
 }
--- lua/podgorniy/plugins/treesitter.lua
+++ lua/podgorniy/plugins/treesitter.lua
@@ -40,6 +40,7 @@ return {
         'query',
         'vimdoc',
         'c',
+        'python',
       },
       incremental_selection = {
         enable = true,
